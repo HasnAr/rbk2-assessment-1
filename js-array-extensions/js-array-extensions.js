d2587d674c7d36db68c7cbff8b789676b99aa325
HasnAr <f.eng.hasn@gmail.com>
2017-01-07 00:18:35 -0800

    [1] js-array-extensions

--- js-array-extensions/js-array-extensions.js
+++ js-array-extensions/js-array-extensions.js
@@ -1,1 +1,17 @@
 // your code here
+
+
+Array.prototype.first = function(){
+	return this[0];
+	//another solution
+	// var temp = this;
+	// return temp.shift();
+}
+
+
+Array.prototype.last = function(){
+	return this[this.length-1];
+	//another solution
+	// var temp = this;
+	// return temp.pop();
+}
